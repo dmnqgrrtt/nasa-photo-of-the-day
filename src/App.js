78c43b5718d2893410593290aeb7668d5d5d78dd
Dominique Garrett <dmnqgrrtt@gmail.com>
2020-04-20 16:05:20 -0400

    styled page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "./App.css";
 import GetPhoto from './components/GetPhoto.js';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  background: blue;
+  border: 5px solid red;
+  border-radius: 50px;
+  padding: 2%;
+  color: white;
+  width: 75%;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+`;
+
 
 function App() {
   return (
     <div className="App">
-      <div className='title'>
-        <h1>NASA PHOTO OF THE DAY</h1>
+      <Wrapper>
+        <Title>NASA PHOTO OF THE DAY</Title>
         <GetPhoto />
-      </div>
+      </Wrapper>
     </div>
   );
 }
--- src/components/ShowPhoto.js
+++ src/components/ShowPhoto.js
@@ -1,17 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const Footer = styled.p`
+    font-weight: bold;
+    color: red;
+`;
+
+const Description = styled.p`
+    width: 65%;
+    margin-top: 3%;
+    border: 2px solid black;    
+    padding: 1%; 
+`;
+
+const PhotoTitle = styled.h2`
+    background: grey;
+    color: white;
+    width: 63.5%;
+    padding-top: 2%;
+    padding-bottom: 2%;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+`;
 
 const ShowPhoto = (props) => {
 
     const photo = props.photo;
     return (
         <div className='show_photo'>
-            <div className='photo_info'>
-                <h2>{photo.title}</h2>
-                <img src={photo.url}/>
-                <p>{photo.explanation}</p>
-            </div>  
+            <Wrapper>
+                <PhotoTitle>{photo.title}</PhotoTitle>
+                <img src={photo.url} alt='picture text'/>
+                <Description>{photo.explanation}</Description>
+            </Wrapper>  
             <div className='copyright'>
-                <p> Copyright {photo.copyright}</p>
+                <Footer> Copyright {photo.copyright}</Footer>
             </div>
         </div>      
     );
